a2d4ac569c7bb3a70f49c755cdacde6d3b8a8ad9
warubert <william.rubert@gmail.com>
2025-08-14 22:52:32 -0300

    slice-type

--- cap4/slice-type/src/main.rs
+++ cap4/slice-type/src/main.rs
@@ -13,3 +13,21 @@ fn first_word(s: &String) -> usize {
 
     s.len()
 }
+
+fn first_word1(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn main() {
+    let mut s = String::from("hello world");
+    let word = first_word(&s);
+    s.clear();    
+}
