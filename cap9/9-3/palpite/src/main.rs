634f36cf57d06855f1f499ddfb63940e69201b10
warubert <william.rubert@gmail.com>
2026-04-14 22:35:39 -0300

    cap9.3

---
+++ cap9/9-3/palpite/src/main.rs
@@ -0,0 +1,23 @@
+pub struct Palpite {
+    valor: u32,
+}
+
+impl Palpite {
+    pub fn new(valor: u32) -> Palpite {
+        if valor < 1 || valor > 100 {
+            panic!("Valor de chute deve ser entre 1 e 100, recebi {}.", valor);
+        }
+
+        Palpite {
+            valor
+        }
+    }
+
+    pub fn valor(&self) -> u32 {
+        self.valor
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
