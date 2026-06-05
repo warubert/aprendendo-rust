9be25ead36ff0a0815281eb501baf640d36617b1
warubert <william.rubert@gmail.com>
2026-06-05 08:30:16 -0300

    tabuada

--- dio/tabuada/src/main.rs
+++ dio/tabuada/src/main.rs
@@ -2,25 +2,98 @@ use std::io;
 
 fn main() {
     loop{
-        println!("Digite o valor da tabuada ou X para sair:");
-        let mut tabuada: String = String::new();
+        println!(r#"
+            Digite uma opção
+            1 - Soma
+            2 - Subtracao
+            3 - Tabuada
+            0 - Sair
+        "#);
+
+        let mut opcao: String = String::new();
     
         io::stdin()
-            .read_line(&mut tabuada)
+            .read_line(&mut opcao)
             .expect("Falha ao ler a entrada");
 
-        match tabuada.trim() {
-            "X" | "x" => {
+        let opcao: u8 = opcao.trim().parse().expect("Por favor, digite um número válido");
+
+        match opcao {
+            1 => println!("Soma: {}", soma()),
+            2 => println!("Subtração: {}", subtracao()),
+            3 => tabuada(),
+            0 => {
                 println!("Saindo...");
                 break;
             },
-            _ => (),
+            _ => println!("Opção inválida"),
         }
+    }
+}
+
+fn soma() -> i32 {
+    println!("Digite o primeiro número:");
+    let mut num1: String = String::new();
     
-        let tabuada: u32 = tabuada.trim().parse().expect("Por favor, digite um número válido");
+    io::stdin()
+        .read_line(&mut num1)
+        .expect("Falha ao ler a entrada");
+
+    let num1: i32 = num1.trim().parse().expect("Por favor, digite um número válido");
+
+    println!("Digite o segundo número:");
+    let mut num2: String = String::new();
     
-        for i in 1..=10 {
-            println!("{} x {} = {}", tabuada, i, tabuada * i);
-        }
+    io::stdin()
+        .read_line(&mut num2)
+        .expect("Falha ao ler a entrada");
+
+    let num2: i32 = num2.trim().parse().expect("Por favor, digite um número válido");
+
+    num1 + num2
+}
+
+fn subtracao() -> i32 {
+    println!("Digite o primeiro número:");
+    let mut num1: String = String::new();
+    
+    io::stdin()
+        .read_line(&mut num1)
+        .expect("Falha ao ler a entrada");
+
+    let num1: i32 = num1.trim().parse().expect("Por favor, digite um número válido");
+
+    println!("Digite o segundo número:");
+    let mut num2: String = String::new();
+    
+    io::stdin()
+        .read_line(&mut num2)
+        .expect("Falha ao ler a entrada");
+
+    let num2: i32 = num2.trim().parse().expect("Por favor, digite um número válido");
+
+    num1 - num2
+}
+
+fn tabuada() {
+    println!("Digite o valor da tabuada ou X para sair:");
+    let mut tabuada: String = String::new();
+    
+    io::stdin()
+        .read_line(&mut tabuada)
+        .expect("Falha ao ler a entrada");
+
+    match tabuada.trim() {
+        "X" | "x" => {
+            println!("Saindo...");
+            return;
+        },
+        _ => (),
+    }
+    
+    let tabuada: u32 = tabuada.trim().parse().expect("Por favor, digite um número válido");
+    
+    for i in 1..=10 {
+        println!("{} x {} = {}", tabuada, i, tabuada * i);
     }
 }
