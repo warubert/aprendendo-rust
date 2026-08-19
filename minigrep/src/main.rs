c88887065917f4084faedcfdeaf10c433a31b441
warubert <william.rubert@gmail.com>
2026-08-19 19:49:22 -0300

    mini grep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,9 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
+
+use minigrep::{Config, run};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,26 +16,8 @@ fn main() {
     println!("Procurando por: {}", config.query);
     println!("No arquivo: {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Algo deu errado ao ler o arquivo");
-
-    println!("{}", contents);
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build (args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Faltam argumentos");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = run(config) {
+        println!("Erro: {}", e);
+        process::exit(1);
     }
 }
