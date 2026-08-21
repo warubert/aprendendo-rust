8a24634d93bc433bc0be6a0ed7410ae3535224a4
warubert <william.rubert@gmail.com>
2026-08-21 00:17:11 -0300

    mini grep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,29 +1,61 @@
 use std::error::Error;
 use std::fs;
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Algo deu errado ao ler o arquivo");
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
 pub struct Config {
     pub query: String,
     pub file_path: String,
 }
 
 impl Config {
-    pub fn build (args: &[String]) -> Result<Config, &'static str> {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            return Err("Faltam argumentos");
+            return Err("not enough arguments");
         }
 
         let query = args[1].clone();
         let file_path = args[2].clone();
 
         Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents: String = fs::read_to_string(config.file_path)?;
+
+    // println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = Vec::new();
+    
+    
+    for line in contents.lines() {
+        if line.contains(query) {
+            //println!("{}", line);
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
