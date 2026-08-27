490effc92a823315335d476335a95b4daeded8f1
warubert <william.rubert@gmail.com>
2026-08-27 19:34:52 -0300

    minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,21 +1,30 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next(); // Ignora o primeiro argumento, que é o nome do programa
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Não foi possível ler o argumento de busca"),
+        };
 
-        Ok(Config { query, file_path })
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Não foi possível ler o arquivo de busca"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
@@ -31,17 +40,22 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results: Vec<&str> = Vec::new();
+    // let mut results: Vec<&str> = Vec::new();
     
     
-    for line in contents.lines() {
-        if line.contains(query) {
-            //println!("{}", line);
-            results.push(line);
-        }
-    }
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         //println!("{}", line);
+    //         results.push(line);
+    //     }
+    // }
 
-    results
+    // results
+
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 #[cfg(test)]
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,14 +1,17 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
 
 use minigrep::{Config, run};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    // let config = Config::build(&args).unwrap_or_else(|err| {
+    //     println!("Problema: {}", err);
+    //     process::exit(1)
+    // });
+
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         println!("Problema: {}", err);
         process::exit(1)
     });
