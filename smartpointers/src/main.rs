22cae78f6f73a9eff484099b2b5f880808fe1c86
warubert <william.rubert@gmail.com>
2026-08-31 13:08:37 -0300

    Box

--- smartpointers/src/main.rs
+++ smartpointers/src/main.rs
@@ -1,7 +1,17 @@
-fn main() {
+use crate::List::{Cons, Nil};
 
+// recursive types with boxes
+//(1, (2, (3, Nil)))
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+fn main() {
     // create a box that points to an integer on the heap
     let b = Box::new(5);
 
     println!("b = {}", b);
+
+    let list: List = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 }
