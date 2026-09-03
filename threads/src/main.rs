0a2fa7c3ab9c0cf695b8dac7950fd38e32eae626
warubert <william.rubert@gmail.com>
2026-09-03 08:24:14 -0300

    threads

---
+++ threads/src/main.rs
@@ -0,0 +1,19 @@
+
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("Thread number {i}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("Main thread number {i}", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+}
