a7cab598fc22bc96b9d0885600364d2d967a3756
warubert <william.rubert@gmail.com>
2026-09-06 12:24:22 -0300

    drop trait

---
+++ drop_trait/src/main.rs
@@ -0,0 +1,25 @@
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+fn main() {
+    let _c = CustomSmartPointer {
+        data: String::from("my data"),
+    };
+
+    let _d = CustomSmartPointer {
+        data: String::from("other data"),
+    };
+
+    println!("CustomSmartPointer created.");
+
+    drop(_c);
+
+    println!("CustomSmartPointer dropped before the end of main.");
+}
